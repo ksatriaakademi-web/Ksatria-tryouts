497733376f32d64b7c85afb36841853b3c02de08
ksatriaakademi-web <ksatriaakademi@gmail.com>
2026-07-17 02:06:50 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,31 @@
+/* ===================================================
+   KSATRIA AKADEMI
+   LOGIN.JS
+=================================================== */
 
+const password = document.getElementById("password");
+const togglePassword = document.getElementById("togglePassword");
+
+if (password && togglePassword) {
+
+    togglePassword.addEventListener("click", function () {
+
+        const icon = this.querySelector("i");
+
+        if (password.type === "password") {
+
+            password.type = "text";
+            icon.classList.remove("bi-eye-fill");
+            icon.classList.add("bi-eye-slash-fill");
+
+        } else {
+
+            password.type = "password";
+            icon.classList.remove("bi-eye-slash-fill");
+            icon.classList.add("bi-eye-fill");
+
+        }
+
+    });
+
+}
