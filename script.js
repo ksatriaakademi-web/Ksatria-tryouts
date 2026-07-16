63a1aba97185431af92e2663544397d536bc6f3e
ksatriaakademi-web <ksatriaakademi@gmail.com>
2026-07-17 01:25:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+/*==================================================
+KSATRIA AKADEMI
+SCRIPT.JS V1
+==================================================*/
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    /*=====================================
+    NAVBAR SCROLL
+    =====================================*/
+
+    const navbar = document.querySelector(".navbar");
+
+    window.addEventListener("scroll", function () {
+
+        if (window.scrollY > 60) {
+
+            navbar.classList.add("navbar-scroll");
+
+        } else {
+
+            navbar.classList.remove("navbar-scroll");
+
+        }
+
+    });
+
+});
