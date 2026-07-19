8707824b692d4a6d492253ce07dcd7de0bd06fcf
ksatriaakademi-web <ksatriaakademi@gmail.com>
2026-07-19 13:46:10 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,24 @@
    FIREBASE CONFIG
 ========================================== */
 
-/*
-Konfigurasi Firebase akan ditambahkan
-pada langkah berikutnya.
-*/
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBOx-jMsZi3byv9wmh7Yrr8IjpS0HiB0-o",
+
+  authDomain: "ksatria-akademi.firebaseapp.com",
+
+  projectId: "ksatria-akademi",
+
+  storageBucket: "ksatria-akademi.firebasestorage.app",
+
+  messagingSenderId: "225142450955",
+
+  appId: "1:225142450955:web:e352e8f237aa7a4ca3100e"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+const db = firebase.firestore();
