e090a03c757039b29ea36500be62faa6d4f2701e
ksatriaakademi-web <ksatriaakademi@gmail.com>
2026-07-19 13:20:09 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,48 +3,7 @@
    FIREBASE CONFIG
 ========================================== */
 
-import { initializeApp }
-from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-
-import {
-getAuth
-}
-from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-
-import {
-getFirestore
-}
-from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
-
-const firebaseConfig = {
-
-apiKey: "ISI_DARI_FIREBASE",
-
-authDomain: "ISI_DARI_FIREBASE",
-
-projectId: "ISI_DARI_FIREBASE",
-
-storageBucket: "ISI_DARI_FIREBASE",
-
-messagingSenderId: "ISI_DARI_FIREBASE",
-
-appId: "ISI_DARI_FIREBASE"
-
-};
-
-const app =
-initializeApp(firebaseConfig);
-
-const auth =
-getAuth(app);
-
-const db =
-getFirestore(app);
-
-export {
-
-auth,
-
-db
-
-};
+/*
+Konfigurasi Firebase akan ditambahkan
+pada langkah berikutnya.
+*/
